22ad0e0aedd18e3b97965463a67e7b366185f864
Hussam Alshawi <hussamahalshawi@gmail.com>
2026-01-15 21:42:00 +0200

    Edit file config

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    MONGO_URI = {
-        'host': os.getenv('MONGO_URI')
-    }
+    MONGO_DB_NAME = os.getenv('MONGO_DB_NAME')
+    MONGO_HOST = os.getenv('MONGO_HOST')
+    MONGO_PORT = int(os.getenv('MONGO_PORT'))
+    # MONGO_URI = {
+    #     'host': os.getenv('MONGO_URI')
+    # }
