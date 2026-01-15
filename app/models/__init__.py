d6bdce8e0c4230fc5d97ac2eafd77372ca4f08dd
Hussam Alshawi <hussamahalshawi@gmail.com>
2026-01-15 21:39:55 +0200

    models file __init__

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .profile import Profile
+# from .education import Education, Language
+# from .learning import Course, SelfLearning
+# from .projects import Project, CodingChallenge
+# from .blog import BlogPost
+# from .goals import CareerGoal, Milestone
+# أضف Testimonial إذا كنت تريده
