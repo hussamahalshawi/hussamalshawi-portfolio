378312766dec71dbaaeabe8fc14648537fe84f8c
Hussam Alshawi <hussamahalshawi@gmail.com>
2026-01-15 21:41:26 +0200

    Edit file init app for database and admin viwe

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
-from flask_login import LoginManager
-
+from flask_admin import Admin
+from app.models.database import init_db
 from config import Config
 from app.routes import portfolio
+from flask_admin.contrib.mongoengine import ModelView
+from app.models import (
+    Profile
+)
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    admin = Admin(app, name='Hussam Portfolio Admin', url='/admin')
+    admin.add_view(ModelView(Profile, name='Profile'))
+    init_db(app)
 
     app.register_blueprint(portfolio)
 
