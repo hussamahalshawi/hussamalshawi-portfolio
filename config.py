f396427d68efe9985ed44b21e9a7a1eba7a8ab70
Hussam Alshawi <hussamahalshawi@gmail.com>
2026-01-16 21:10:10 +0200

    add code for path abspath

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ class Config:
     MONGO_DB_NAME = os.getenv('MONGO_DB_NAME')
     MONGO_HOST = os.getenv('MONGO_HOST')
     MONGO_PORT = int(os.getenv('MONGO_PORT'))
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    UPLOAD_PATH = os.path.join(BASE_DIR, 'app', 'static', 'images')
     # MONGO_URI = {
     #     'host': os.getenv('MONGO_URI')
     # }
