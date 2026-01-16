bd625ec9961c54e055af666823bd129df91bb93c
Hussam Alshawi <hussamahalshawi@gmail.com>
2026-01-16 21:08:59 +0200

    edit viwe model

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_admin import Admin
+from flask_admin.form import FileUploadField
+from app.admin_views import ProfileView
 from app.models.database import init_db
 from config import Config
 from app.routes import portfolio
@@ -9,11 +11,12 @@ from app.models import (
 )
 
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     admin = Admin(app, name='Hussam Portfolio Admin', url='/admin')
-    admin.add_view(ModelView(Profile, name='Profile'))
+    admin.add_view(ProfileView(Profile, name='Profile'))
     init_db(app)
 
     app.register_blueprint(portfolio)
